89070c3e9ad377bdf341cd3edaf69216e0ac86c4
codingdani <96432557+codingdani@users.noreply.github.com>
2024-09-25 11:50:22 +0200

    rename _uri to _baseUri, change mint function, integrate TokenId operations

--- Factory.sol
+++ Factory.sol
@@ -5,37 +5,38 @@ import "./TokenContract.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CustomERC1155Factory is Ownable {
-    event TokenContractCreated(address indexed creator, address indexed tokenContract);
+    event TokenContractCreated(address indexed creator, address indexed tokenContract, string name);
 
     address[] public createdContracts;
     mapping(address => address[]) public creatorToContracts;
 
-    function createTokenContract(string memory _uri) public returns (address) {
-        TokenContract newContract = new TokenContract(_uri);
+    function createTokenContract(string memory _baseUri, string memory _name) public returns (address) {
+        TokenContract newContract = new TokenContract(_baseUri, _name);
         newContract.transferOwnership(msg.sender);
         createdContracts.push(address(newContract));
         creatorToContracts[msg.sender].push(address(newContract));
-        emit TokenContractCreated(msg.sender, address(newContract));
+        emit TokenContractCreated(msg.sender, address(newContract), _name);
         return address(newContract);
     }
 
     function createTokenContractAndMint(
-        string memory _uri,
-        address _account,
+        string memory _baseUri,
+        string memory _name,
+        address _receiver,
         uint256 _id,
         uint256 _amount,
         string memory _tokenURI,
         bytes memory _data
-        ) public returns (address) {
-        TokenContract newContract = new TokenContract(_uri);
+    ) public returns (address) {
+        TokenContract newContract = new TokenContract(_baseUri, _name);
         newContract.transferOwnership(msg.sender);
-    // Mint tokens
-        newContract.mint(_account, _id, _amount, _tokenURI, _data);
-    // Add contract to tracking arrays and mappings
+        // Mint tokens
+        newContract.mint(_receiver, _id, _amount, _tokenURI, _data);
+        // Add contract to tracking arrays and mappings
         createdContracts.push(address(newContract));
         creatorToContracts[msg.sender].push(address(newContract));
-    // Emit event
-        emit TokenContractCreated(msg.sender, address(newContract));
+        // Emit event
+        emit TokenContractCreated(msg.sender, address(newContract), _name);
         return address(newContract);
     }
 
--- TokenContract.sol
+++ TokenContract.sol
@@ -9,6 +9,16 @@ import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 contract TokenContract is ERC1155, Ownable, Initializable {
     using Strings for uint256;
 
+    string public name;
+
+    uint256[] private allTokenIds;
+
+    struct TokenDetails {
+    uint256 totalSupply;
+    string uri;
+    }
+    mapping(uint256 => TokenDetails) private tokenDetails;
+
     // maps token ID to metadata URI
     mapping(uint256 => string) private tokenURIs;
 
@@ -20,7 +30,9 @@ contract TokenContract is ERC1155, Ownable, Initializable {
         uint256 percentage;
     }
 
-    constructor(string memory _uri) ERC1155(_uri) {}
+    constructor(string memory _baseUri, string memory _name) ERC1155(_baseUri) {
+        name = _name;
+    }
 
     function mint(
         address _receiver, 
@@ -29,17 +41,67 @@ contract TokenContract is ERC1155, Ownable, Initializable {
         string memory _newuri, 
         bytes memory _data
         ) public onlyOwner {
+        if (tokenDetails[_id].totalSupply == 0) {
+                allTokenIds.push(_id);
+            }
+        tokenDetails[_id].totalSupply += _amount;
+        tokenDetails[_id].uri = _newuri;
         _mint(_receiver, _id, _amount, _data);
         setTokenURI(_id, _newuri);
     }
 
     function mintBatch(
-        address _receiver, 
-        uint256[] memory _ids, 
-        uint256[] memory _amounts, 
+        address _receiver,
+        uint256[] memory _ids,
+        uint256[] memory _amounts,
+        string[] memory _uris,
         bytes memory _data
-        ) public onlyOwner {
+    ) public onlyOwner {
+        require(_ids.length == _amounts.length && _ids.length == _uris.length, "Arrays length mismatch");
+        for (uint256 i = 0; i < _ids.length; i++) {
+            uint256 id = _ids[i];
+            uint256 amount = _amounts[i];
+            string memory uri = _uris[i];
+            if (_tokenDetails[id].totalSupply == 0) {
+                _allTokenIds.push(id);
+            }
+            _tokenDetails[id].totalSupply += amount;
+              _tokenDetails[id].uri = uri;
+            setTokenURI(id, uri);
+        }
         _mintBatch(_receiver, _ids, _amounts, _data);
+    }
+
+    function getAllTokenIds() public view returns (uint256[] memory) {
+    return allTokenIds; 
+    }   
+
+    function getTokenDetails(uint256 _tokenId) public view returns (
+        uint256 totalSupply, 
+        string memory tokenUri
+        ) {
+    TokenDetails memory details = tokenDetails[_tokenId];
+    return (details.totalSupply, details.uri);
+    }
+
+    function getAllTokensWithDetails() public view returns (
+    uint256[] memory _ids,
+    uint256[] memory _supplies,
+    string[] memory _uris
+    ) {
+        uint256 length = _allTokenIds.length;
+        ids = new uint256[](length);
+        supplies = new uint256[](length);
+        uris = new string[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            uint256 id = _allTokenIds[i];
+            ids[i] = id;
+            supplies[i] = _tokenDetails[id].totalSupply;
+            uris[i] = _tokenDetails[id].uri;
+        }
+
+        return (ids, supplies, uris);
     }
 
     function setTokenRoyalty(
@@ -73,5 +135,9 @@ contract TokenContract is ERC1155, Ownable, Initializable {
             return tokenURI;
         }
         return super.uri(_tokenId);
+    }
+
+    function setName(string memory _newName) public onlyOwner {
+        name = _newName;
     }
 }

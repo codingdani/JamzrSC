66eb7cbd71db378a0c19bbec1348677b9aa2878d
codingdani <96432557+codingdani@users.noreply.github.com>
2024-11-04 12:01:11 +0100

    changes after REMIX test, cleanup

--- Factory.sol
+++ Factory.sol
@@ -12,9 +12,9 @@ contract CustomERC1155Factory is Ownable {
 
     constructor(address initialOwner) Ownable(initialOwner) {}
 
-    function createTokenContract(string memory _baseURI, string memory _name) public returns (address) {
+    function createTokenContract(string memory _baseURI, string memory _name) public 
+    returns (address) {
         TokenContract newContract = new TokenContract(_name, _baseURI, msg.sender);
-        newContract.transferOwnership(msg.sender);
         createdContracts.push(address(newContract));
         creatorToContracts[msg.sender].push(address(newContract));
         emit TokenContractCreated(msg.sender, address(newContract), _name);
@@ -27,14 +27,23 @@ contract CustomERC1155Factory is Ownable {
         address _receiver,
         uint256 _amount,
         string memory _tokenURI,
+        address _royaltyRecipient,
+        uint256 _royaltyPercentage,
         bytes memory _data
     ) public returns (address, uint256) {
-        TokenContract newContract = new TokenContract(_name, _baseURI, msg.sender);
-        newContract.transferOwnership(msg.sender);
+        TokenContract newContract = new TokenContract(_name, _baseURI, address(this));
         // Mint tokens
-        uint256 tokenId = newContract.mint(_receiver, _amount, _tokenURI, _data);
+        uint256 tokenId = newContract.mint(
+            _receiver, 
+            _amount, 
+            _tokenURI, 
+            _royaltyRecipient, 
+            _royaltyPercentage, 
+            _data);
         // Add contract to tracking arrays and mappings
         createdContracts.push(address(newContract));
+        // Transfer ownership to the caller (msg.sender)
+        newContract.transferOwnership(msg.sender);
         creatorToContracts[msg.sender].push(address(newContract));
         // Emit event
         emit TokenContractCreated(msg.sender, address(newContract), _name);
@@ -45,20 +54,24 @@ contract CustomERC1155Factory is Ownable {
         return createdContracts.length;
     }
 
-    function getContractAddress(uint256 _index) public view returns (address) {
+    function getContractAddress(uint256 _index) public view 
+    returns (address) {
         require(_index < createdContracts.length, "Index out of bounds");
         return createdContracts[_index];
     }
 
-    function getCreatorContractCount(address _creator) public view returns (uint256) {
+    function getCreatorContractCount(address _creator) public view 
+    returns (uint256) {
         return creatorToContracts[_creator].length;
     }
 
-    function getAllContractsByCreator(address _creator) public view returns (address[] memory) {
+    function getAllContractsByCreator(address _creator) public view 
+    returns (address[] memory) {
         return creatorToContracts[_creator];
     }
 
-    function getCreatorContractAddress(address _creator, uint256 _index) public view returns (address) {
+    function getCreatorContractAddress(address _creator, uint256 _index) public view 
+    returns (address) {
         require(_index < creatorToContracts[_creator].length, "Index out of bounds");
         return creatorToContracts[_creator][_index];
     }
--- TokenContract.sol
+++ TokenContract.sol
@@ -17,6 +17,8 @@ contract TokenContract is ERC1155, Ownable {
     struct TokenDetails {
     uint256 totalSupply;
     string uri;
+    address royaltyRecipient;
+    uint256 royaltyPercentage;
     }
     //maps token ID to TokenDetails
     mapping(uint256 => TokenDetails) private tokenDetails;
@@ -24,15 +26,9 @@ contract TokenContract is ERC1155, Ownable {
     // maps token ID to metadata URI
     mapping(uint256 => string) private tokenURIs;
 
-    // maps token ID to royalty information
-    mapping(uint256 => RoyaltyInfo) private royalties;
-
-    struct RoyaltyInfo {
-        address recipient;
-        uint256 percentage;
-    }
-
-    constructor(string memory _name, string memory _baseURI, address _owner) ERC1155(_baseURI) Ownable(_owner) {
+    constructor(string memory _name, string memory _baseURI, address _owner) 
+    ERC1155(_baseURI) 
+    Ownable(_owner) {
         name = _name;
     }
 
@@ -40,15 +36,20 @@ contract TokenContract is ERC1155, Ownable {
         address _receiver,
         uint256 _amount,
         string memory _tokenURI,
+        address _royaltyRecipient,
+        uint256 _royaltyPercentage,
         bytes memory _data
     ) external onlyOwner returns (uint256) {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(_royaltyPercentage <= 10000, "Royalty percentage cannot exceed 100%");
         uint256 tokenId = nextTokenId++;
         tokenDetails[tokenId] = TokenDetails({
             totalSupply: _amount,
-            uri: _tokenURI
+            uri: _tokenURI,
+            royaltyRecipient: _royaltyRecipient,
+            royaltyPercentage: _royaltyPercentage
         });
         _mint(_receiver, tokenId, _amount, _data);
-        setTokenURI(tokenId, _tokenURI);
         emit TokenMinted(tokenId, _receiver, _amount, _tokenURI);
         return tokenId;
     }
@@ -57,25 +58,34 @@ contract TokenContract is ERC1155, Ownable {
         address _receiver,
         uint256[] memory _amounts,
         string[] memory _uris,
+        address[] memory _royaltyRecipients,
+        uint256[] memory _royaltyPercentages,
         bytes memory _data
     ) external onlyOwner {
-        require(_amounts.length == _uris.length, "Arrays length mismatch");
+        require(_amounts.length == _uris.length && 
+                _amounts.length == _royaltyRecipients.length && 
+                _amounts.length == _royaltyPercentages.length, 
+                "Arrays length mismatch");
         uint256[] memory newIds = new uint256[](_amounts.length);
         for (uint256 i = 0; i < _amounts.length; i++) {
+            require(_amounts[i] > 0, "Amount must be greater than 0");
+            require(_royaltyPercentages[i] <= 10000, 
+            "Royalty percentage cannot exceed 100%");
             uint256 newId = nextTokenId++;
             newIds[i] = newId;
             tokenDetails[newId] = TokenDetails({
                 totalSupply: _amounts[i],
-                uri: _uris[i]
+                uri: _uris[i],
+                royaltyRecipient: _royaltyRecipients[i],
+                royaltyPercentage: _royaltyPercentages[i]
             });
-            setTokenURI(newId, _uris[i]);
             emit TokenMinted(newId, _receiver, _amounts[i], _uris[i]);
         }
         _mintBatch(_receiver, newIds, _amounts, _data);
-    }   
+    }
 
     function getTotalUniqueTokens() external view returns (uint256) {
-    return nextTokenId > 0 ? nextTokenId - 1 : 0;
+    return nextTokenId > 1 ? nextTokenId - 1 : 1;
     }   
 
     function getTokenDetails(uint256 _tokenId) external view returns (
@@ -110,29 +120,20 @@ contract TokenContract is ERC1155, Ownable {
         uint256 _tokenId, 
         address _recipient, 
         uint256 _percentage
-        ) public onlyOwner {
+    ) external onlyOwner {
         require(_percentage <= 10000, "Royalty percentage cannot exceed 100%");
-        royalties[_tokenId] = RoyaltyInfo(_recipient, _percentage);
+        TokenDetails storage details = tokenDetails[_tokenId];
+        details.royaltyRecipient = _recipient;
+        details.royaltyPercentage = _percentage;
     }
 
-    function royaltyInfo(
-        uint256 _tokenId, 
-        uint256 _salePrice
-        ) public view returns (
-            address receiver, 
-            uint256 royaltyAmount
-            ) {
-        RoyaltyInfo memory royalty = royalties[_tokenId];
-        return (royalty.recipient, (_salePrice * royalty.percentage) / 10000);
-    }
-
-    function setTokenURI(uint256 _tokenId, string memory _newURI) private {
-        tokenURIs[_tokenId] = _newURI;
+    function setTokenURI(uint256 _tokenId, string memory _newURI) external onlyOwner {
+        tokenDetails[_tokenId].uri = _newURI;
     }
 
     function uri(uint256 _tokenId) public view virtual override 
     returns (string memory) {
-        string memory tokenURI = tokenURIs[_tokenId];
+        string memory tokenURI = tokenDetails[_tokenId].uri;
         if (bytes(tokenURI).length > 0) {
             return tokenURI;
         }
